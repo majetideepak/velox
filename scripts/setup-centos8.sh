feac36df069189e5e691da9930ed0b02ed137f6b
Deepak Majeti <deepak.majeti@ibm.com>
2024-01-19 14:34:42 -0500

    Reuse setup-centos8.sh in setup-circlei.sh

--- scripts/setup-centos8.sh
+++ scripts/setup-centos8.sh
@@ -23,6 +23,7 @@ NPROC=$(getconf _NPROCESSORS_ONLN)
 export CFLAGS=$(get_cxx_flags $CPU_TARGET)  # Used by LZO.
 export CXXFLAGS=$CFLAGS  # Used by boost.
 export CPPFLAGS=$CFLAGS  # Used by LZO.
+CMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE:-Release}"
 
 function dnf_install {
   dnf install -y -q --setopt=install_weak_deps=False "$@"
@@ -32,15 +33,12 @@ dnf_install epel-release dnf-plugins-core # For ccache, ninja
 dnf config-manager --set-enabled powertools
 dnf_install ninja-build ccache gcc-toolset-9 git wget which libevent-devel \
   openssl-devel re2-devel libzstd-devel lz4-devel double-conversion-devel \
-  libdwarf-devel curl-devel cmake libicu-devel
+  libdwarf-devel curl-devel libicu-devel
 
 dnf remove -y gflags
 
 # Required for Thrift
-dnf_install autoconf automake libtool bison flex python3
-
-# Required for google-cloud-storage
-dnf_install curl-devel c-ares-devel
+dnf_install autoconf automake libtool bison flex python3 libsodium-devel
 
 dnf_install conda
 
@@ -48,9 +46,9 @@ dnf_install conda
 source /opt/rh/gcc-toolset-9/enable || exit 1
 set -u
 
-function cmake_install_deps {
+function cmake_install {
   cmake -B "$1-build" -GNinja -DCMAKE_CXX_STANDARD=17 \
-    -DCMAKE_CXX_FLAGS="${CFLAGS}" -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DCMAKE_BUILD_TYPE=Release -Wno-dev "$@"
+    -DCMAKE_CXX_FLAGS="${CFLAGS}" -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DCMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE}" -Wno-dev "$@"
   ninja -C "$1-build" install
 }
 
@@ -61,6 +59,8 @@ function wget_and_untar {
   wget -q --max-redirect 3 -O - "${URL}" | tar -xz -C "${DIR}" --strip-components=1
 }
 
+# untar cmake binary release directly to /usr.
+wget_and_untar https://github.com/Kitware/CMake/releases/download/v3.17.5/cmake-3.17.5-Linux-x86_64.tar.gz /usr &
 
 # Fetch sources.
 wget_and_untar https://github.com/gflags/gflags/archive/v2.2.2.tar.gz gflags &
@@ -69,6 +69,14 @@ wget_and_untar http://www.oberhumer.com/opensource/lzo/download/lzo-2.10.tar.gz 
 wget_and_untar https://boostorg.jfrog.io/artifactory/main/release/1.72.0/source/boost_1_72_0.tar.gz boost &
 wget_and_untar https://github.com/google/snappy/archive/1.1.8.tar.gz snappy &
 wget_and_untar https://github.com/fmtlib/fmt/archive/8.0.1.tar.gz fmt &
+wget_and_untar https://github.com/protocolbuffers/protobuf/releases/download/v21.4/protobuf-all-21.4.tar.gz protobuf &
+
+FB_OS_VERSION="v2022.11.14.00"
+
+wget_and_untar https://github.com/facebook/folly/archive/${FB_OS_VERSION}.tar.gz folly &
+wget_and_untar https://github.com/facebookincubator/fizz/archive/refs/tags/${FB_OS_VERSION}.tar.gz fizz &
+wget_and_untar https://github.com/facebook/wangle/archive/refs/tags/${FB_OS_VERSION}.tar.gz wangle &
+wget_and_untar https://github.com/facebook/fbthrift/archive/refs/tags/${FB_OS_VERSION}.tar.gz fbthrift &
 
 wait  # For cmake and source downloads to complete.
 
@@ -86,9 +94,23 @@ wait  # For cmake and source downloads to complete.
   ./b2 "-j$(nproc)" -d0 install threading=multi
 )
 
-cmake_install_deps gflags -DBUILD_SHARED_LIBS=ON -DBUILD_STATIC_LIBS=ON -DBUILD_gflags_LIB=ON -DLIB_SUFFIX=64 -DCMAKE_INSTALL_PREFIX:PATH=/usr
-cmake_install_deps glog -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX:PATH=/usr
-cmake_install_deps snappy -DSNAPPY_BUILD_TESTS=OFF
-cmake_install_deps fmt -DFMT_TEST=OFF
+(
+  cd protobuf
+  ./configure --prefix=/usr
+  make "-j${NPROC}"
+  make install
+  ldconfig
+)
+
+cmake_install gflags -DBUILD_SHARED_LIBS=ON -DBUILD_STATIC_LIBS=ON -DBUILD_gflags_LIB=ON -DLIB_SUFFIX=64 -DCMAKE_INSTALL_PREFIX:PATH=/usr
+cmake_install glog -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX:PATH=/usr
+cmake_install snappy -DSNAPPY_BUILD_TESTS=OFF
+cmake_install fmt -DFMT_TEST=OFF
+cmake_install folly -DFOLLY_HAVE_INT128_T=ON
+
+cmake_install fizz/fizz -DBUILD_TESTS=OFF
+cmake_install wangle/wangle -DBUILD_TESTS=OFF
+cmake_install fbthrift -Denable_tests=OFF
+# cmake_install ranges-v3
 
 dnf clean all
